549491a6576d99ffde161a36f09551c43ac6d31d
Benswhites <56427173+Benswhites@users.noreply.github.com>
2021-11-11 09:17:42 +0100

    Add files via upload

--- swarmManager/taskClass.py
+++ swarmManager/taskClass.py
@@ -13,13 +13,13 @@ def AmountBricks():
 
 def ProdBotEat():
     job = len(ArrayLists.arrayOfProc)
-    State = ArrayLists.State[0]
+    Order = ["Not Complete", "Not Complete", "Not Complete", "Not Complete", "Not Complete", "Not Complete", "Not Complete",]
     while job != 0:
         newamount = 50  # ændre til hvor meget der kan være i the process robot
         for i in range(len(ArrayLists.arrayOfProc)):
             prodTaskColor = random.choice(ArrayLists.arrayAvailableColor)
-            if prodTaskColor == ArrayLists.arrayAvailableColor[0] and State == ArrayLists.State[0]:
-                State = ArrayLists.State[1]
+            if prodTaskColor == ArrayLists.arrayAvailableColor[0] and ArrayLists.State[0] == "Not Occupied" and Order[0] == "Not Complete":
+                ArrayLists.State[0] = "Occupied"
                 totalAmountOfColor0 = ArrayLists.arrayAvailableBricks[0]
                 prodTaskHowManyOfColor0 = random.choice(range(0,5))
                 newAmountAfterBrickGone0 = totalAmountOfColor0- prodTaskHowManyOfColor0
@@ -28,11 +28,12 @@ def ProdBotEat():
                     ArrayLists.arrayAvailableBricks[0] = newamount
                 time.sleep(2)
                 job -= 1
-                State = ArrayLists.State[0]
+                ArrayLists.State[0] = "Not Occupied"
+                Order[0] = "Complete"
 
 
-            elif prodTaskColor == ArrayLists.arrayAvailableColor[1] and State == ArrayLists.State[0]:
-                State = ArrayLists.State[1]
+            elif prodTaskColor == ArrayLists.arrayAvailableColor[1] and ArrayLists.State[1] == "Not Occupied" and Order[1] == "Not Complete":
+                ArrayLists.State[1] = "Occupied"
                 totalAmountOfColor1 = ArrayLists.arrayAvailableBricks[1]
                 prodTaskHowManyOfColor1 = random.choice(range(0,5))
                 newAmountAfterBrickGone1 = totalAmountOfColor1 - prodTaskHowManyOfColor1
@@ -41,10 +42,11 @@ def ProdBotEat():
                     ArrayLists.arrayAvailableBricks[1] = newamount
                 time.sleep(2)
                 job -= 1
-                State = ArrayLists.State[0]
+                ArrayLists.State[1] = "Not Occupied"
+                Order[1] = "Complete"
 
-            elif prodTaskColor == ArrayLists.arrayAvailableColor[2] and State == ArrayLists.State[0]:
-                State = ArrayLists.State[1]
+            elif prodTaskColor == ArrayLists.arrayAvailableColor[2] and ArrayLists.State[2] == "Not Occupied" and Order[2] == "Not Complete":
+                ArrayLists.State[2] = "Occupied"
                 totalAmountOfColor2= ArrayLists.arrayAvailableBricks[2]
                 prodTaskHowManyOfColor2 = random.choice(range(0,5))
                 newAmountAfterBrickGone2 = totalAmountOfColor2 - prodTaskHowManyOfColor2
@@ -53,22 +55,26 @@ def ProdBotEat():
                     ArrayLists.arrayAvailableBricks[2] = newamount
                 time.sleep(2)
                 job -= 1
-                State = ArrayLists.State[0]
+                ArrayLists.State[2] = "Not Occupied"
+                Order[2] = "Complete"
 
-            elif prodTaskColor == ArrayLists.arrayAvailableColor[3] and State == ArrayLists.State[0]:
-                State = ArrayLists.State[1]
-                totalAmountOfColor3 = ArrayLists.arrayAvailableBricks[3]
-                prodTaskHowManyOfColor3 = random.choice(range(0,5))
-                newAmountAfterBrickGone3 = totalAmountOfColor3 - prodTaskHowManyOfColor3
-                ArrayLists.arrayAvailableBricks[3] = newAmountAfterBrickGone3
-                if ArrayLists.arrayAvailableBricks[3] < 5: #ændre til det antal bricks der skal til for en ordre
-                    ArrayLists.arrayAvailableBricks[3] = newamount
-                time.sleep(2)
-                job -= 1
-                State = ArrayLists.State[0]
+            elif prodTaskColor == ArrayLists.arrayAvailableColor[3]:
+                if ArrayLists.State[3] == "Not Occupied":
+                    if Order[3] == "Not Complete":
+                        ArrayLists.State[3] = "Occupied"
+                        totalAmountOfColor3 = ArrayLists.arrayAvailableBricks[3]
+                        prodTaskHowManyOfColor3 = random.choice(range(0,5))
+                        newAmountAfterBrickGone3 = totalAmountOfColor3 - prodTaskHowManyOfColor3
+                        ArrayLists.arrayAvailableBricks[3] = newAmountAfterBrickGone3
+                        if ArrayLists.arrayAvailableBricks[3] < 5: #ændre til det antal bricks der skal til for en ordre
+                            ArrayLists.arrayAvailableBricks[3] = newamount
+                        time.sleep(2)
+                        job -= 1
+                        ArrayLists.State[3] = "Not Occupied"
+                        Order[3] = "Complete"
 
-            elif prodTaskColor == ArrayLists.arrayAvailableColor[4] and State == ArrayLists.State[0]:
-                State = ArrayLists.State[1]
+            elif prodTaskColor == ArrayLists.arrayAvailableColor[4] and ArrayLists.State[4] == "Not Occupied" and Order[4] == "Not Complete":
+                ArrayLists.State[4] = "Occupied"
                 totalAmountOfColor4 = ArrayLists.arrayAvailableBricks[4]
                 prodTaskHowManyOfColor4 = random.choice(range(0,5))
                 newAmountAfterBrickGone4 = totalAmountOfColor4 - prodTaskHowManyOfColor4
@@ -77,10 +83,11 @@ def ProdBotEat():
                     ArrayLists.arrayAvailableBricks[4] = newamount
                 time.sleep(2)
                 job -= 1
-                State = ArrayLists.State[0]
+                ArrayLists.State[4] = "Not Occupied"
+                Order[4] = "Complete"
 
-            elif prodTaskColor == ArrayLists.arrayAvailableColor[5] and State == ArrayLists.State[0]:
-                State = ArrayLists.State[1]
+            elif prodTaskColor == ArrayLists.arrayAvailableColor[5] and ArrayLists.State[5] == "Not Occupied" and Order[5] == "Not Complete":
+                ArrayLists.State[5] = "Occupied"
                 totalAmountOfColor5 = ArrayLists.arrayAvailableBricks[5]
                 prodTaskHowManyOfColor5 = random.choice(range(0,5))
                 newAmountAfterBrickGone5 = totalAmountOfColor5 - prodTaskHowManyOfColor5
@@ -89,10 +96,11 @@ def ProdBotEat():
                     ArrayLists.arrayAvailableBricks[5] = newamount
                 time.sleep(2)
                 job -= 1
-                State = ArrayLists.State[0]
+                ArrayLists.State[5] = "Not Occupied"
+                Order[5] = "Complete"
 
-            elif prodTaskColor == ArrayLists.arrayAvailableColor[6] and State == ArrayLists.State[0]:
-                State = ArrayLists.State[1]
+            elif prodTaskColor == ArrayLists.arrayAvailableColor[6] and ArrayLists.State[6] == "Not Occupied" and Order[6] == "Not Complete":
+                ArrayLists.State[6] = "Occupied"
                 totalAmountOfColor6 = ArrayLists.arrayAvailableBricks[6]
                 prodTaskHowManyOfColor6 = random.choice(range(0,5))
                 newAmountAfterBrickGone6 = totalAmountOfColor6 - prodTaskHowManyOfColor6
@@ -101,7 +109,8 @@ def ProdBotEat():
                     ArrayLists.arrayAvailableBricks[6] = newamount
                 time.sleep(2)
                 job -= 1
-                State = ArrayLists.State[0]
+                ArrayLists.State[6] = "Not Occupied"
+                Order[6] = "Complete"
 
     print("Process agents colors: ", ArrayLists.arrayAvailableColor, "Amount of bricks in each color:", ArrayLists.arrayAvailableBricks)
 
